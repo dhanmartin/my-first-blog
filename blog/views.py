688e574c1de8daa5267049cebe65c3670698f26d
Dhan Barbarona <dhan@newlogic.com>
2022-07-21 10:52:36 +0800

    Squash Commit

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 def post_list(request):
+    print("Test2")
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
