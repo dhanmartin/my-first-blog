4f591417849ac95c989981d7ec64330475c64141
Dhan Barbarona <dhan@newlogic.com>
2022-08-01 17:22:01 +0800

    latest master

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 def post_list(request):
+    print("Test5")
+    print("Test6")
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
